e2c98bb17db2e2f46fe80247aee3a209fb48b55c
JoonhyungBae <jh.bae@kaist.ac.kr>
2024-10-22 01:45:43 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <h1>The ISMIR 2025 website is under development</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  text-align: center;
+  background-color: #f5f5f5;
+}
+
+h1 {
+  font-family: Arial, sans-serif;
+  color: #333;
+}
+</style>

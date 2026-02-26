03e7995c20ae26decb6410d12aa9a3430c937d03
Joonhyung Bae <jh.bae@kaist.ac.kr>
2026-02-26 16:50:44 +0900

    Update logo image import case and remove unused FooterPage component from AboutLogoPage; clean up yarn.lock by removing obsolete entries.

--- src/views/AboutLogoPage.vue
+++ src/views/AboutLogoPage.vue
@@ -1,19 +1,16 @@
 <template>
   <div>
     <SectionAboutLogo />
-    <FooterPage />
   </div>
 </template>
 
 <script>
 import SectionAboutLogo from "@/components/section/SectionAboutLogo.vue";
-import FooterPage from "@/components/FooterPage.vue";
 
 export default {
   name: "AboutLogoPage",
   components: {
     SectionAboutLogo,
-    FooterPage,
   },
 };
 </script>
